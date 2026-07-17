cdecc0a93bea7894bc3b68b1ca46e2bc766af918
saya-k <1192625769@qq.com>
2026-07-17 15:23:42 +0800

    Set postcard text top to 80 percent

--- 8thwall-test/image-target-config.js
+++ 8thwall-test/image-target-config.js
@@ -327,7 +327,7 @@
       .postcard-title {
         position: absolute;
         left: 50%;
-        top: 40%;
+        top: 80%;
         transform: translate(-50%, -50%);
         display: flex;
         flex-direction: column;
